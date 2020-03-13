5d4b898c257889181022bbb181fde8ac9be641af
Wellington Umbelino <wellingtonsi@alu.ufc.br>
2020-03-13 09:24:45 -0300

    classes

---
+++ src/components/classes/MinhaClasse.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class MinhaClasse extends Component {
+  render() {
+    return (
+      <div>Oi, eu sou o {this.props.nome}</div>
+    )
+  }
+}
--- src/components/filho_pai/Pai.jsx
+++ src/components/filho_pai/Pai.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Filho from './Filho';
 
 export default props => {
-  const msgRecebida = (msg) => alert('Recebi: ' + msg)
+  //const msgRecebida = (msg) => alert('Recebi: ' + msg)
+  const msgRecebida = (msg) => alert(`Recebi: ${msg}`)
   return (
     <div>
       <Filho notificarPai={msgRecebida} label='Oi pai!' />
